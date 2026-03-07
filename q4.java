92185713e8f4217ad87c789c2074a43a74f93b2e
CodeAaks <28aakritikaran@gmail.com>
2026-03-07 19:33:10 +0530

    Added q4 program

---
+++ q4.java
@@ -0,0 +1,17 @@
+class q4 {
+    public static void main(String[] args) {
+
+        float number1 = 2;
+        float number2 = 4;
+
+        float addition = number1 + number2;
+        float subtraction = number1 - number2;
+        float multiplication = number1 * number2;
+        float division = number1 / number2;
+
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+    }
+}
