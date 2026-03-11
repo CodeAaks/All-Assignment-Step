ca3175e16ee4de2c25ad9ec72f831dee6ff690e3
CodeAaks <28aakritikaran@gmail.com>
2026-03-11 08:32:34 +0530

    added

--- q2.java
+++ q2.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-public class q2 {
+public class q2{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int number1, number2, number3;
 
-        System.out.print("Enter first number: ");
+        System.out.print("Enter number1: ");
         number1 = sc.nextInt();
 
-        System.out.print("Enter second number: ");
+        System.out.print("Enter number2: ");
         number2 = sc.nextInt();
 
-        System.out.print("Enter third number: ");
+        System.out.print("Enter number3: ");
         number3 = sc.nextInt();
 
         if (number1 < number2 && number1 < number3) {
